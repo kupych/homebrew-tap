fa8a241478f64f1b6f863655f69b6f10b0d47dc6
Stefan Kupych <stefan@hifyre.com>
2025-10-29 01:32:57 -0400

    Added formula

---
+++ rapid.rb
@@ -0,0 +1,17 @@
+class Rapid < Formula
+  desc "Rapid API Dialogue - A REPL for APIs"
+  homepage "https://github.com/kupych/rapid"
+  url "https://github.com/kupych/rapid/archive/v0.0.4.tar.gz"
+  sha256 "b62307fd4048413edaa08ab9fc50dd20a1db6bbe9924a3eb532d7bbab0060efa"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"rapid"
+  end
+
+  test do
+    assert_match "RAPID", shell_output("${bin}/rapid 2>&1", 1)
+  end
+end
