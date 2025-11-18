fb53b6e48f9e03b9c336eb6408ee80f4af08ac9e
Stefan Kupych <stefan@kupy.ch>
2025-11-18 01:11:49 -0500

    Update rapid to v0.2.7

--- rapid.rb
+++ rapid.rb
@@ -1,8 +1,8 @@
 class Rapid < Formula
   desc "Rapid API Dialogue - A REPL for APIs"
   homepage "https://github.com/kupych/rapid"
-  url "https://github.com/kupych/rapid/archive/v0.2.6.tar.gz"
-  sha256 "fd0f913b74e190d2ba1c69c8c452cf4ff1a3bc8a3f7c56bbbfbe80d7c92d16cd"
+  url "https://github.com/kupych/rapid/archive/v0.2.7.tar.gz"
+  sha256 "d405cc8d00154cd873ea9a38d8df22d38bb533ee90bdf321b0f8532419073e52"
   license "MIT"
 
   depends_on "go" => :build
