bed6a0ff879783391f409c07b871e56375973830
Stefan Kupych <stefan@kupy.ch>
2025-12-06 20:50:12 -0500

    Update rapid to v0.2.9

--- rapid.rb
+++ rapid.rb
@@ -1,8 +1,8 @@
 class Rapid < Formula
   desc "Rapid API Dialogue - A REPL for APIs"
   homepage "https://github.com/kupych/rapid"
-  url "https://github.com/kupych/rapid/archive/v0.2.8.tar.gz"
-  sha256 "8ab4f850ace6683cd7062894b4b3add887d23f2f5f60157b620b2ff53642fe1a"
+  url "https://github.com/kupych/rapid/archive/v0.2.9.tar.gz"
+  sha256 "df34faa6ba454e204fffdf682337246a32858565b40f524fafd671b706c3d89e"
   license "MIT"
 
   depends_on "go" => :build
